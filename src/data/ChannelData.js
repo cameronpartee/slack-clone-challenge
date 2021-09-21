b42a480e84b6d3a56d3ba64b9d37cefbe51d5cf8
cameronpartee <cameronepartee@gmail.com>
2021-09-20 20:59:03 -0400

    add files to hold app data

---
+++ src/data/ChannelData.js
@@ -0,0 +1,13 @@
+import LoyaltyIcon from "@material-ui/icons/Loyalty";
+import EmojiEmotionsIcon from "@material-ui/icons/EmojiEmotions";
+
+export const channelItemsData = [
+  {
+    icon: <LoyaltyIcon />,
+    text: "Channel 1",
+  },
+  {
+    icon: <EmojiEmotionsIcon />,
+    text: "Channel 2",
+  },
+];
