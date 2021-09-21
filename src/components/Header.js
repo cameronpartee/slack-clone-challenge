d8ceb63ce1063d088d18ce1706f408a0f8fb70b4
cameronpartee <cameronepartee@gmail.com>
2021-09-20 20:58:35 -0400

    finish styling header component

--- src/components/Header.js
+++ src/components/Header.js
@@ -31,21 +31,63 @@ const Container = styled.div`
   color: white;
   display: flex;
   align-items: center;
-  justify-content: space-between;
+  justify-content: center;
+  position: relative;
+  box-shadow: 0 1px 0 0 rgb(255 255 255 / 10%);
 `;
 
 const Main = styled.div`
   display: flex;
+  margin-left: 16px;
+  margin-right: 16px;
 `;
 const UserContainer = styled.div`
   display: flex;
   align-items: center;
+  padding-right: 16px;
+  position: absolute;
+  right: 0;
 `;
 
-const SearchContainer = styled.div``;
+const SearchContainer = styled.div`
+  min-width: 400px;
+  margin-left: 16px;
+  margin-right: 16px;
+`;
 
-const Search = styled.div``;
+const Search = styled.div`
+  box-shadow: inset 0 0 0 1px rgb(104 74 104);
+  width: 100%;
+  border-radius: 6px;
+  display: flex;
+  align-items: center;
 
-const Name = styled.div``;
+  input {
+    background-color: transparent;
+    border: none;
+    padding-left: 8px;
+    padding-right: 8px;
+    color: white;
+    padding-top: 4px;
+    padding-bottom: 4px;
+  }
 
-const UserImage = styled.div``;
+  input:focus {
+    outline: none;
+  }
+`;
+
+const Name = styled.div`
+  padding-right: 16px;
+`;
+
+const UserImage = styled.div`
+  width: 28px;
+  height: 28px;
+  border: 2px solid white;
+  border-radius: 3px;
+
+  img {
+    width: 100%;
+  }
+`;
