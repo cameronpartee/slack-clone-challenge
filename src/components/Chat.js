c11b94c9e87bd00d2da5e5e0e8134c9238260e97
cameronpartee <cameronepartee@gmail.com>
2021-10-23 21:06:52 -0400

    add send message functionality

--- src/components/Chat.js
+++ src/components/Chat.js
@@ -2,13 +2,57 @@ import styled from "styled-components";
 import InfoOutlinedIcon from "@material-ui/icons/InfoOutlined";
 import ChatInput from "./ChatInput";
 import ChatMessage from "./ChatMessage";
+import db from "../firebase";
+import { useParams } from "react-router-dom";
+import { useEffect, useState } from "react";
+import firebase from "firebase";
 
-const Chat = () => {
+const Chat = ({ user }) => {
+  let { channelId } = useParams();
+  const [channel, setChannel] = useState("");
+  const [messages, setMessages] = useState([]);
+
+  const getMessages = () => {
+    db.collection("rooms")
+      .doc(channelId)
+      .collection("messages")
+      .orderBy("timestamp", "asc")
+      .onSnapshot((snapshot) => {
+        let messages = snapshot.docs.map((doc) => doc.data());
+        setMessages(messages);
+      });
+  };
+
+  const sendMessage = (text) => {
+    if (channelId) {
+      let payload = {
+        text: text,
+        timestamp: firebase.firestore.Timestamp.now(),
+        user: user.name,
+        userImage: user.photo,
+      };
+      db.collection("rooms").doc(channelId).collection("messages").add(payload);
+    }
+  };
+
+  const getChannel = () => {
+    db.collection("rooms")
+      .doc(channelId)
+      .onSnapshot((snapshot) => {
+        setChannel(snapshot.data());
+      });
+  };
+
+  useEffect(() => {
+    getChannel();
+    getMessages();
+  }, [channelId]);
+
   return (
     <Container>
       <Header>
         <Channel>
-          <ChannelName># clever</ChannelName>
+          <ChannelName># {channel && channel.name}</ChannelName>
           <ChannelInfo>
             Company-wide announcements and work-based matters
           </ChannelInfo>
@@ -19,9 +63,17 @@ const Chat = () => {
         </ChannelDetails>
       </Header>
       <MessageContainer>
-        <ChatMessage />
+        {messages.length > 0 &&
+          messages.map((data, index) => (
+            <ChatMessage
+              text={data.text}
+              name={data.user}
+              image={data.userImage}
+              timestamp={data.timestamp}
+            />
+          ))}
       </MessageContainer>
-      <ChatInput />
+      <ChatInput sendMessage={sendMessage} />
     </Container>
   );
 };
@@ -31,6 +83,7 @@ export default Chat;
 const Container = styled.div`
   display: grid;
   grid-template-rows: 64px auto min-content;
+  min-height: 0;
 `;
 
 const Channel = styled.div``;
@@ -50,7 +103,11 @@ const Header = styled.div`
   justify-content: space-between;
 `;
 
-const MessageContainer = styled.div``;
+const MessageContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  overflow-y: scroll;
+`;
 
 const ChannelName = styled.div`
   font-weight: 700;
